1f96ff0a0c61e18b80ff2243d1e718e6f2e11bd5
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-03-06 18:04:22 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,16 +6,16 @@ import requests
 
 
 def top_ten(subreddit)
-"""
-ten hot posts listed
-"""
+    """
+    ten hot posts listed
+    """
 
-url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-response = requests.get(url, allow_reddits=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_reddits=False)
 
-if response.status_code != 200:
-    print(None)
-    return
-posts = response.json()["data"]["immature"]
-for post in posts
-    print(post["data"]["title"])
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()["data"]["immature"]
+    for post in posts
+        print(post["data"]["title"])
