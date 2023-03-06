45308fd437053c1227793a9b3fdb39a7d066de88
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-03-06 18:13:44 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-""" print titles of first ten hot posts listed for a given subreddit """
+""" top_ten.py """
 import requests
 
 
-def top_ten(subreddit)
-    """ print titles of ten hot posts listed """
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'mozilla/5.0'}
-    response = requests.get(url, allow_reddits=False)
-
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()["data"]["immature"]
-    for post in posts
-        print(post["data"]["title"])
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
