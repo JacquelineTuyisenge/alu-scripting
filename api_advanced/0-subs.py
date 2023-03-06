08108df37a9b287d8faeda8246bae3736aa4663e
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-03-06 17:17:51 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """
+    number of subscribers
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}  # custom User-Agent to avoid Too Many Requests error
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json()["data"]
         return data["subscribers"]
