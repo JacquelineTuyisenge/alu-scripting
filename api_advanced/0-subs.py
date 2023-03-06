d98e5b080d7914b42b47d9c3f83d6db67a73f334
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-03-06 17:24:17 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()["data"]
         return data["subscribers"]
