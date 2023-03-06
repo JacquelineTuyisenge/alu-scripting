2949fafa8d81eb022460ec790eaf54843727c36c
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-03-06 19:09:05 +0200

    Update 2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" recursive functions and list of hot articles """
+""" write recursive functions and list of hot articles """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ list with titles of hot articles """
+    """ returns list with titles of hot articles """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 100, "after": after}
