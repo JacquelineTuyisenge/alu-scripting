6f5ee0a47de382fa6e189041b87e305c3b662543
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-03-06 18:09:52 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,6 +7,7 @@ def top_ten(subreddit)
     """ print titles of ten hot posts listed """
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'mozilla/5.0'}
     response = requests.get(url, allow_reddits=False)
 
     if response.status_code != 200:
